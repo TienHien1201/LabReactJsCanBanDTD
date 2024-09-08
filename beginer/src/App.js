f1ac82753d5e78fac891d150339149a168000744
TienHien1201 <phamtienhien08072018@gmai.com>
2024-09-08 11:37:01 +0700

    The Firt Push

---
+++ beginer/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg'
+import './App.css'
+//Đây là file component tổng (component là các khối xây dựng cơ bản của giao diện người dùng. Nó giống như các hàm JavaScript nhưng cho phép bạn tách nhỏ giao diện thành các phần độc lập, có thể tái sử dụng và component tổng là file chứa các file component con)
+function App() {
+  return (
+    //Đây là cú pháp JSX tương tự như cú pháp của HTML
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
+          Learn React
+        </a>
+      </header>
+    </div>
+  )
+}
+
+export default App
---
+++ beginer/src/index.js
@@ -0,0 +1,18 @@
+//Đây là file import file component tổng file component tổng là cái file app.js để render component tổng ra HTML và đưa hết tất cả các HTML này vào thẻ div có ID là root
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals()
